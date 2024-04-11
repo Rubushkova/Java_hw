6c7b8544ab00015fa7990f9924dfa179dbc5e41a
Tel-ran.de <rubushkova333@gmail.com>
2024-04-11 20:35:52 +0200

    creat file

--- Java_homework/src/hm3/Main.java
+++ Java_homework/src/hm3/Main.java
@@ -15,9 +15,24 @@ public class Main {
         System.out.println( "Итоговое слово : " + resultWord);
 
 
+
         Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        long firstNumber = scanner.nextLong();
+        System.out.println("Введите второе число: ");
+        long secondNumber = scanner.nextLong();
+
+        System.out.println("Сумма: " + (firstNumber + secondNumber));
+        System.out.println("Разность: " + (firstNumber - secondNumber));
+        System.out.println("Произведение: " + (firstNumber * secondNumber));
+        System.out.println("Частное: " + (firstNumber / secondNumber));
+
+
+
+
+        Scanner scanner2 = new Scanner(System.in);
         System.out.println(" Введите ваше имя ");
-        String name = scanner1.nextLine();
+        String name = scanner2.nextLine();
         System.out.println("Добрый день, " + name + "!");
         System.out.println("Ваше имя начинается с символа " + name.charAt(0));
         System.out.println("Ваше имя заканчивается на символ " + name.charAt(name.length() - 1 ));
